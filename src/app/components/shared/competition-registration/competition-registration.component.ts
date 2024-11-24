2708ce103a9bb823d2e2d634c26dbee0c48da608
Ako <akaki.vashakidze1@gmail.com>
2024-11-24 09:10:50 +0400

    add athlete autocomplete start

--- src/app/components/shared/competition-registration/competition-registration.component.ts
+++ src/app/components/shared/competition-registration/competition-registration.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { CompetitionService } from '../../../services/competition.service';
 import { EventDetails, Race } from '../../../interfaces/interfaces';
 import { ActivatedRoute } from '@angular/router';
@@ -8,34 +8,62 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { map, Observable, startWith } from 'rxjs';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-competition-registration',
   standalone: true,
-  imports: [CommonModule,LoaderSpinnerComponent, MatTabsModule, MatCardModule, MatIconModule, MatFormFieldModule],
+  imports: [
+    CommonModule,
+    MatAutocompleteModule,
+    LoaderSpinnerComponent,
+    ReactiveFormsModule,
+    MatTabsModule,
+    MatCardModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
   templateUrl: './competition-registration.component.html',
-  styleUrl: './competition-registration.component.scss'
+  styleUrls: ['./competition-registration.component.scss'],
 })
-export class CompetitionRegistrationComponent {
-  event = signal<EventDetails | null>(null)
-  chosenRace:Race | null = null;
-  constructor(private competitionService:CompetitionService,private route: ActivatedRoute){
+export class CompetitionRegistrationComponent implements OnInit {
+  event = signal<EventDetails | null>(null);
+  myControl = new FormControl('');
+  chosenRace: Race | null = null;
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions!: Observable<string[]>;
+
+  constructor(
+    private competitionService: CompetitionService,
+    private route: ActivatedRoute
+  ) {
     const eventId = this.route.snapshot.paramMap.get('id') || '';
     competitionService.getEventDetails(eventId).subscribe(item => {
-      console.log(item)
-      this.event.set(item)
-    })
-    console.log(this.chosenRace)
+      this.event.set(item);
+    });
   }
 
-  chooseRace(race:Race){
+  ngOnInit(): void {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || ''))
+    );
+  }
+
+  chooseRace(race: Race) {
     this.chosenRace = race;
-    console.log(this.chosenRace)
   }
 
-  onTabChange(action:any){
+  onTabChange(action:any) {
     this.chosenRace = null;
   }
 
-
+  private _filter(value: string): string[] {
+    const filterValue = value?.toLowerCase() || '';
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
 }
