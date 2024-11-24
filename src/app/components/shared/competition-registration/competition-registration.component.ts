0efe2c496b8c5c1aee0df0f0db49ff0ae68e28a9
Ako <akaki.vashakidze1@gmail.com>
2024-11-24 09:01:28 +0400

    s

--- src/app/components/shared/competition-registration/competition-registration.component.ts
+++ src/app/components/shared/competition-registration/competition-registration.component.ts
@@ -1,26 +1,41 @@
 import { Component, signal } from '@angular/core';
 import { CompetitionService } from '../../../services/competition.service';
-import { EventDetails } from '../../../interfaces/interfaces';
+import { EventDetails, Race } from '../../../interfaces/interfaces';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { LoaderSpinnerComponent } from '../loader-spinner/loader-spinner.component';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-competition-registration',
   standalone: true,
-  imports: [CommonModule,LoaderSpinnerComponent, MatTabsModule, MatCardModule],
+  imports: [CommonModule,LoaderSpinnerComponent, MatTabsModule, MatCardModule, MatIconModule, MatFormFieldModule],
   templateUrl: './competition-registration.component.html',
   styleUrl: './competition-registration.component.scss'
 })
 export class CompetitionRegistrationComponent {
   event = signal<EventDetails | null>(null)
+  chosenRace:Race | null = null;
   constructor(private competitionService:CompetitionService,private route: ActivatedRoute){
     const eventId = this.route.snapshot.paramMap.get('id') || '';
     competitionService.getEventDetails(eventId).subscribe(item => {
       console.log(item)
       this.event.set(item)
     })
+    console.log(this.chosenRace)
   }
+
+  chooseRace(race:Race){
+    this.chosenRace = race;
+    console.log(this.chosenRace)
+  }
+
+  onTabChange(action:any){
+    this.chosenRace = null;
+  }
+
+
 }
